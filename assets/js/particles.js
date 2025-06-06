d11abb426a774e50493e5721aa5ea1133a3e795c
nicolabcraft <nicolabcraft.2@gmail.com>
2025-06-06 14:41:49 +0200

    feat: :sparkles: Add particles background effect to enhance visual appeal; include particles.js for dynamic background

---
+++ assets/js/particles.js
@@ -0,0 +1,63 @@
+tsParticles.load("tsparticles", {
+  "fullScreen": {
+    "enable": false
+  },
+  "particles": {
+    "number": {
+      "value": 200,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": true,
+      "anim": {
+        "enable": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false
+      }
+    },
+    "line_linked": {
+      "enable": false
+    },
+    "move": {
+      "enable": true,
+      "speed": 0.5,
+      "direction": "bottom",
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false
+      },
+      "onclick": {
+        "enable": false
+      },
+      "resize": true
+    }
+  },
+  "retina_detect": true
+});
